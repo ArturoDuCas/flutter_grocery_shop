98ef9fca8db935d3e676142eb4213a84e8023bfd
ArturoDuCas <a00833516@tec.mx>
2024-01-11 19:01:26 -0600

    feat: implement some methods to cart_model for handling the cart items

--- lib/model/cart_model.dart
+++ lib/model/cart_model.dart
@@ -10,5 +10,32 @@ class CartModel extends ChangeNotifier {
     ["Chicken", "2.99", "lib/images/chicken.png", Colors.orange],
   ];
 
+  // list of cart items
+  List _cartItems = [];
+
   get shopItems => _shopItems;
+
+  get cartItems => _cartItems;
+
+  // add items to the cart
+  void addItemToCart(int index) {
+    _cartItems.add(_shopItems[index]);
+    notifyListeners();
+  }
+
+  // remove items from the art
+  void removeItemsFromCart(int index) {
+    _cartItems.removeAt(index);
+    notifyListeners();
+  }
+
+  // calculate total price
+  String calculateTotalPrice() {
+    double totalPrice = 0;
+    for (int i = 0; i < _cartItems.length; i++) {
+      totalPrice += double.parse(_cartItems[i][1]);
+    }
+
+    return totalPrice.toStringAsFixed(2);
+  }
 }
